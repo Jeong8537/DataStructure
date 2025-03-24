cc3277147db50900c807161213a7ee8327e1342a
Jeong8537 <jeong85373546@gmail.com>
2025-03-24 10:51:17 +0900

    v0.4 | array!

--- week03.py
+++ week03.py
@@ -1,7 +1,9 @@
-array = [11, 9, -77, 8]
+import array
+arr = array.array('f',[11, 9, -77, 8])
 # print(f"{array[0]}, {id(array[0])}")
 # print(f"{array[1]}, {id(array[1])}")
 # print(f"{array[2]}, {id(array[2])}")
 # print(f"{array[-1]}, {id(array[-1])}")
-for i in range(len(array)):
-    print(f"{array[i]}, {id(array[i])}")
+for i in range(len(arr)):
+    print(f"{arr[i]:3}, {id(arr[i])}")
+
