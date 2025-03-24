7d9a49c5869325bcabe38ba624a1c3390d94c1e8
Jeong8537 <jeong85373546@gmail.com>
2025-03-24 12:15:12 +0900

    intersection!

--- week03.py
+++ week03.py
@@ -1,11 +1,10 @@
-def intersection(l1 ,l2):
-    l3 = list()
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return l3
+def inters(l1 ,l2):
+    s1 = set(l1)
+    s2 = set(l2)
+
+    return(s1 & s2)
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 31, 38, 7, 27, 19, 41]
 
-print(intersection(l1, l2))
+print(inters(l1, l2))
