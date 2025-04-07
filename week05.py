242ab3600888c626b8a89963752fbf91660d88b4
Jeong8537 <jeong85373546@gmail.com>
2025-04-07 11:34:34 +0900

    V1.6 | Stack(Linked List)!

--- week05.py
+++ week05.py
@@ -1,38 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
 class Stack:
     def __init__(self):
-        self.items = list()
+        self.top = None
 
-    def push(self, item):
-        self.items.append(item)
+    def push(self, data):
+        node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            node.next = self.top
+            self.top = node
 
     def pop(self):
-        return self.items.pop()
+        if self.top is None:
+            # raise IndexError('pop from empty stack!')
+            return f"Stack is empty! Noting to delete | {self.top}"
+        popped_node = self.top
+        self.top = self.top.link
+        return popped_node.data
 
-    def size(self):
-        return len(self.items)
-
-    def is_empty(self):
-        return len(self.items) == 0
-
-    def peek(self):
-        return self.items[-1]
-
-    def all_peek(self):
-        return self.items[:]
 
 s1 = Stack()
-s2 = Stack()
-# print(s1.is_empty())
+print(s1.pop())
 
-s1.push("DataStruct")
-print(s1.is_empty())
-print(s2.is_empty())
-
-s1.push("DataBase")
-print(s1.size())
-# print(s2.size())
-
-print(s1.peek())
-print(s1.all_peek())
-
+s1.push("Data_struct")
+print(s1.pop())
 print(s1.pop())
