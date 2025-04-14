261be54f9922ee51dc3615607b04c97a2e6d2b6c
Jeong8537 <jeong85373546@gmail.com>
2025-04-14 11:42:58 +0900

    v1.9 | Queue(Basic)!

---
+++ week06.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self._size = 0
+
+    def enqueue(self, data):
+        self._size = self._size + 1
+        node = Node(data)
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.link = node
+            self.rear = node
+
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError("Queue is Empty!")
+        self._size = self._size - 1
+        temp = self.front
+        self.front = self.front.link
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+Q = Queue()
+
+Q.enqueue("자료구조")
+Q.enqueue("데이터베이스")
+print(Q._size, Q.front.data, Q.rear.data)
+
+Q.dequeue()
+Q.dequeue()
+print(Q._size, Q.front, Q.rear)
